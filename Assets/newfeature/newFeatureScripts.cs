128ae6ae7671e5f69ec25a24bed173fa00e11698
Micah West <micahdavidwest@gmail.com>
2016-08-15 11:04:28 -0600

    Added new feature

---
+++ Assets/newfeature/newFeatureScripts.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class newFeatureScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("new features");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
