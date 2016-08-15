036a53ca802534870adcba2db6a79594669b65aa
Micah West <micahdavidwest@gmail.com>
2016-08-15 10:23:17 -0600

    Inital Project Commit

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	 
+		Debug.Log ("started");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
